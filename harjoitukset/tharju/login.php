a4c5db4cc87a26a9be3d7cc99b7e86050c858ac8
Toni Harju <centria.otek@gmail.com>
2014-02-12 15:29:14 +0200

    Added some example files

---
+++ harjoitukset/tharju/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Log In - PHP</title>
+  </head>
+  <body>
+    <?php
+
+    if( $_POST != null ) {
+
+    ?>
+    <div>Entered username: <?php echo $_POST[ 'username' ]; ?></div>
+    <div>Entered password: <?php echo $_POST[ 'password' ]; ?></div>
+    <?php
+    
+    } else {
+
+    ?>
+    <div>Error</div>
+    <?php
+
+    }
+
+    ?>
+  </body>
+</html>
