0b2bcffec759179ccf60126d80cbfaf2ef2e8480
ohjelmistoprojekti <centria.otek@gmail.com>
2014-03-26 12:57:03 +0200

    Create config.php

---
+++ sql/mvc/config.php
@@ -0,0 +1,19 @@
+<?php
+
+$config = array(
+
+	'theme'		=> 'default',
+	
+	'mysql'		=> array(
+	
+		'host'		=> 'localhost',
+		'user'		=> '',
+		'pass'		=> '',
+		'database'	=> 'Group1',
+		'prefix'	=> ''
+	
+	)
+
+);
+
+?>
